1e47aca0cd63174aa6eeec2d7a6670b151703cec
Fabricio Camargo <fmcfdev@outllok.com>
2025-04-02 22:04:23 -0300

    feat: add table and format transaction type with badge

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,13 +1,22 @@
+import { ArrowUpDown } from "lucide-react";
+import { Button } from "../_components/ui/button";
 import { db } from "../_lib/prisma";
+import { DataTable } from "../_components/ui/data-table";
+import { transactionColumns } from "./_columns";
 
 const TransactionsPage = async () => {
   const transactions = await db.transaction.findMany({});
 
   return (
-    <div>
-      {transactions.map((transaction) => (
-        <div key={transaction.id}>{transaction.name}</div>
-      ))}
+    <div className="space-y-6 p-6">
+      <div className="flex w-full items-center justify-between">
+        <h1>Transações</h1>
+        <Button className="rounded-full">
+          Adicionar transação
+          <ArrowUpDown />
+        </Button>
+      </div>
+      <DataTable columns={transactionColumns} data={transactions} />
     </div>
   );
 };
