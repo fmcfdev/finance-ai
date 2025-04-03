be47ec02a99d96a38f4fef1fbfc828435097e09d
Fabricio Camargo <fmcfdev@outllok.com>
2025-04-02 22:47:26 -0300

    feat: ensure transaction table labels are rendered property

---
+++ app/transactions/data/category-labels.tsx
@@ -0,0 +1,11 @@
+export const TRANSACTION_CATEGORY_LABELS = {
+  EDUCATION: "Educação",
+  ENTERTAINMENT: "Entretenimento",
+  FOOD: "Alimentação",
+  HEALTH: "Saúde",
+  HOUSING: "Moradia",
+  OTHER: "Outros",
+  SALARY: "Salário",
+  TRANSPORTATION: "TTransaporte",
+  UTILITY: "Utilidades",
+};
