c9c24df1d7c5f2b588aab5e60946cacfa93e063d
Fabricio Camargo <fmcfdev@outlook.com>
2025-05-02 15:14:48 -0300

    fix: ensure pages are protected

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -3,9 +3,19 @@ import { DataTable } from "../_components/ui/data-table";
 import { transactionColumns } from "./_columns";
 import AddTransactionButton from "../_components/add-transaction-button";
 import NavBar from "../_components/navbar";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 const TransactionsPage = async () => {
-  const transactions = await db.transaction.findMany({});
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }
+  const transactions = await db.transaction.findMany({
+    where: {
+      userId, //mesma coisa que --> userId: userId já que o nome é o mesmo
+    },
+  });
 
   return (
     <div>
